db953adf067467463fcec04724872a4047fbda85
saim-alam1 <allbehassansaim@gmail.com>
2025-05-04 02:38:44 +0600

    Added NavBar in Left Aside And Loaded Data Dynamaticaly

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router';
 
 const CategoryNews = () => {
+
+  const { id } = useParams();
+  console.log(id);
+
   return (
     <div>
-      <h1>Category News</h1>
+      <h1>Category News - {id}</h1>
     </div>
   );
 };
