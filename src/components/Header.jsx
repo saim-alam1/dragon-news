10f7c66386075439aae803d8f94a7a254fa5b0b1
saim-alam1 <allbehassansaim@gmail.com>
2025-05-03 15:27:21 +0600

    Used Date-FNS To Display Date

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from '../assets/logo.png';
+import { format } from 'date-fns';
 
 const Header = () => {
   return (
     <div className='flex justify-center flex-col items-center mt-12'>
       <img src={logo} alt="" />
-      <p className='mt-5 mb-2.5 text-[18px]'>Journalism Without Fear or Favour</p>
-      <p className='text-xl'>Sunday, November 27, 2025</p>
+      <p className='mt-5 mb-2.5 text-[18px] text-accent'>Journalism Without Fear or Favour</p>
+      <p className='text-xl font-semibold text-accent'>{format(new Date(), "EEEE, LLLL d, yyyy")}</p>
     </div>
   );
 };
