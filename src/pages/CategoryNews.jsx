cdb781c28975d85542fd965424384853f4bc118a
saim-alam1 <allbehassansaim@gmail.com>
2025-05-04 03:57:35 +0600

    Loaded News Data Dynamaticaly As Per News Nav

--- src/pages/CategoryNews.jsx
+++ src/pages/CategoryNews.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import { useParams } from 'react-router';
+import React, { useEffect, useState } from 'react';
+import { useLoaderData, useParams } from 'react-router';
 
 const CategoryNews = () => {
 
   const { id } = useParams();
-  console.log(id);
+  const data = useLoaderData();
+
+  const [categoryNews, setCategoryNews] = useState([]);
+
+  // console.log(id, data);
+  useEffect(() => {
+    if (id == '0') {
+      setCategoryNews(data);
+      return;
+    } else if (id == '1') {
+      const filteredNews = data.filter(news => news.others.is_today_pick == true);
+      setCategoryNews(filteredNews);
+    } else {
+      const filteredNews = data.filter(news => news.category_id == id);
+      // console.log(filteredNews);
+      setCategoryNews(filteredNews);
+    }
+  }, [data, id])
 
   return (
     <div>
-      <h1>Category News - {id}</h1>
+      <h1>Total - {categoryNews.length} News Found</h1>
     </div>
   );
 };
